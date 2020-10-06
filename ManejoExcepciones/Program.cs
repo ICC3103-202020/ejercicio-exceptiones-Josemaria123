c4b7397d8485022582c1b51c531029b993b313c1
Josemaria Saldias <jsaldias@miuandes.cl>
2020-10-06 10:32:05 -0300

    Ejercicio Exceociones

---
+++ ManejoExcepciones/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using ManejoExcepciones.Properties;
+
+namespace ManejoExcepciones
+{
+    class MainClass
+    {
+        public static List<string> lista_consumibles = new List<string>() { "Fertilizante", "Herbicida", "Fungicida" };
+
+        public static void Main(string[] args)
+        {
+            chequearLista();
+        }
+
+        public static void chequearLista()
+        {
+            Console.WriteLine("Ingrese un consumible para buscar");
+            string choice = Console.ReadLine();
+
+            if (lista_consumibles.Contains(choice))
+            {
+                Console.WriteLine("Si existe el consumible en la lista");
+            }
+
+            else
+            {
+                throw new ExcepcionPersonalizada("El consumible no existe");
+            }
+        }
+    }
+}
---
+++ ManejoExcepciones/Properties/ExcepcionPersonalizada.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ManejoExcepciones.Properties
+{
+    public class ExcepcionPersonalizada:ApplicationException
+    {
+
+        public ExcepcionPersonalizada(string message):base("No se encontro el consumible")
+        {
+        }
+    }
+}
